8883130bfc931e5303d99401c45a6d01dc9c5a9b
Luiz Bills <luizbills@pm.me>
2026-03-12 18:09:51 -0300

    fix legal comment

--- src/_web.js
+++ src/_web.js
@@ -1,4 +1,4 @@
 import plugin from "./index.js"
 
-/*! Joystick plugin for litecanvas v0.4.2 by Luiz Bills | MIT Licensed */
+/*! Joystick plugin for Litecanvas by Luiz Bills | MIT Licensed */
 window.pluginJoystick = plugin
